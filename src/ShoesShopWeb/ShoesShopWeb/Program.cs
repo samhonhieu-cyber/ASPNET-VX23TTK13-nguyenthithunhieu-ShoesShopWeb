5553f36f10ac1c699b869c8870e2f7acbb5bfb1a
samhonhieu-cyber <samhonhieu@gmail.com>
2025-11-16 18:24:05 +0700

    Configure dependency injection and seed data

--- src/ShoesShopWeb/ShoesShopWeb/Program.cs
+++ src/ShoesShopWeb/ShoesShopWeb/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using ShoesShopWeb.DAL.Data;
+using ShoesShopWeb.DAL.Interfaces;
+using ShoesShopWeb.DAL.Repositories;
+using ShoesShopWeb.DAL.Seed;
+using ShoesShopWeb.BLL.Interfaces;
+using ShoesShopWeb.BLL.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +12,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Register Unit of Work
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Register Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+// Register Services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// Seed Database
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await DbInitializer.SeedAsync(context);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
